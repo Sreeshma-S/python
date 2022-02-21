1227ad7e3300bc8d227d12f179a9520c5a175a2f
Sreeshma-S <86057467+Sreeshma-S@users.noreply.github.com>
2022-02-21 08:47:34 +0530

    Delete exercise_2 directory

--- exercise_2/5_reverse.py
+++
@@ -1,9 +0,0 @@
-# Question 5:
-# Reverse the string given below.
-# name = ‘Bridgelabz Organisation’
-# Expected Output:
-# 'noitasinagrO zbalegdirBl'
-
-
-name = "Bridgelabz Organisation"
-print(name[: : -1])
